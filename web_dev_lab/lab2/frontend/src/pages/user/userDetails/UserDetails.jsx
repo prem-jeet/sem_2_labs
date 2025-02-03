7e87681f203c675ebf331f23cc3e97f3f94492ba
Premjeet <jeet4300795@gmail.com>
2025-02-03 21:00:48 +0530

    added proxy for cors

--- web_dev_lab/lab2/frontend/src/pages/user/userDetails/UserDetails.jsx
+++ web_dev_lab/lab2/frontend/src/pages/user/userDetails/UserDetails.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const UserDetails = () => {
+  const url = import.meta.env.VITE_API_URL + "/user";
+  const token = localStorage.getItem("token");
+  const [user, setUser] = useState({});
+
+  useEffect(() => async () => {
+    const req = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: token, // This is required for JSON
+      },
+    });
+    const res = await req.json();
+    console.log(res);
+  });
+
   return <div>UsereDetails</div>;
 };
 
--- web_dev_lab/lab2/frontend/vite.config.js
+++ web_dev_lab/lab2/frontend/vite.config.js
@@ -3,7 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": { changeOrigin: true, target: "http://localhost:55000" },
+    },
+  },
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
